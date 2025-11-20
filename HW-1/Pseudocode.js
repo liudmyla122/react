9a66f91ba8276d8ecfd6c6d1a798119fe81f982e
liudmyla122 <afina.palada555@gmail.com>
2025-11-20 04:48:22 -0800

    Add hw-1

---
+++ HW-1/Pseudocode.js
@@ -0,0 +1,13 @@
+function sumThreeNumbers(num1, num2, num3) {
+  let sum = num1 + num2 + num3
+  console.log('Сумма чисел:', sum)
+
+  return sum
+}
+
+const num1 = 10
+const num2 = 25
+const num3 = 7
+
+console.log(`Входные числа: ${num1}, ${num2}, ${num3}`)
+sumThreeNumbers(num1, num2, num3)
